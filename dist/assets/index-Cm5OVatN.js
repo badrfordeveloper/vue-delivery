2c7937be234a44dbe806d1d53bc04be44dadeeca
badrfordeveloper <mrbadrjeddab@gmail.com>
2025-02-21 13:15:29 +0100

    build

---
+++ dist/assets/index-Cm5OVatN.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{V as r}from"./VCard-BG-sLHPS.js";import{c as o,o as t,b as c}from"./index-DzV6yVlR.js";import"./VAvatar-B_166d6N.js";import"./VImg-DpWbwdj8.js";import"./VCardText-DH-YrV8w.js";const n={};function a(i,m){return t(),o("div",null,[c(r,{title:"Bienvenue"})])}const u=e(n,[["render",a]]);export{u as default};
